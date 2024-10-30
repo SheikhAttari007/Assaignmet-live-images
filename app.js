ea95cb56ff80f5d9dd3892441ebf4e639f9acf04
SheikhAttari007 <abdulhaseeb1926@gmail.com>
2024-10-30 19:08:18 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,52 @@
+// var carImg  = document.getElementById("carImg")
+// function imageHandler() {
+//   console.log("carImg" , carImg.src)
+//     // carImg.src = "./images/car2.webp"
+//     // carImg.src = "./images/car22.gif"
+//     carImg.src = "./images/game2.gif"
+// }
+
+// function imageOutHandler() {
+//     console.log("carImg" , carImg.src)
+//       carImg.src = "./images/game1.png"
+//   }
+
+var stnd =document.getElementById("stnd")
+
+function inn( ){
+    stnd.src="./images/pngImage.jfif"
+    console.log (stnd.src)
+  
+    stnd.src="./images/image2.gif"
+}
+
+function out( ){
+    console.log (stnd.src)
+    stnd.src="./images/pngImage.jfif"
+}
+
+var car =document.getElementById("car")
+
+function carUp(){
+    car.src = "./images/carImage.jfif"
+ console.log(car.src)
+ car.src = "./images720ca5bb5a0a70ea0fb60e7db560c952.gif"
+ 
+}
+function carOut (){
+    console.log (car.src)
+    car.src = "./images/carImage.jfif"
+}
+
+// function innOne( ){
+//     stndOne.src="./images/pngImage.jfif"
+//     console.log (stndOne.src)
+  
+//     stnd.src="./images/image2.gif"
+// }
+
+// function outOne( ){
+//     console.log (stndOne.src)
+//     stndOne.src="./images/pngImage.jfif"
+// }
+
