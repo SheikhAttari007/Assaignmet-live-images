c7ad871a0d2f36284a1e432651935ef00e73a189
SheikhAttari007 <abdulhaseeb1926@gmail.com>
2024-10-31 23:04:23 +0500

    new changes

--- app.js
+++ app.js
@@ -14,23 +14,22 @@
 var stnd =document.getElementById("stnd")
 
 function inn( ){
-    stnd.src="./images/pngImage.jfif"
-    console.log (stnd.src)
+    
   
-    stnd.src="./images/image2.gif"
+    stnd.src="./images/spider2.gif"
 }
 
 function out( ){
     console.log (stnd.src)
-    stnd.src="./images/pngImage.jfif"
+    stnd.src="./images/spider1.webp"
 }
 
 var car =document.getElementById("car")
 
 function carUp(){
-    car.src = "./images/carImage.jfif"
+    car.src = "./images/liveCar.gif"
  console.log(car.src)
- car.src = "./images720ca5bb5a0a70ea0fb60e7db560c952.gif"
+ 
  
 }
 function carOut (){
@@ -38,15 +37,34 @@ function carOut (){
     car.src = "./images/carImage.jfif"
 }
 
-// function innOne( ){
-//     stndOne.src="./images/pngImage.jfif"
-//     console.log (stndOne.src)
-  
-//     stnd.src="./images/image2.gif"
-// }
 
-// function outOne( ){
-//     console.log (stndOne.src)
-//     stndOne.src="./images/pngImage.jfif"
-// }
+ function hInn(){
+    var hwoarang = document.getElementById("hwoarang")
+    console.log(hwoarang)
+    hwoarang.src = "./images/Hwoarang2.gif"
+ }
 
+ function hOut(){
+    hwoarang.src = "././images/Hwoarang 1.jfif"
+    console.log(hwoarang.src)
+ }
+
+ function bulbOn(){
+    var bulb = document.getElementById("bulb")
+    console.log(bulb)
+    bulb.src = "./images/bulb 2.gif"
+ }
+  function bulbOf(){
+    bulb.src = "./images/bulb 1.jpg"
+    console.log(bulb.src)
+  }
+
+function sunSet (){
+    var sun = document.getElementById("sun")
+    console.log(sun)
+    sun.src = "./images/sun2.gif"
+}
+
+function sunStop(){
+    sun.src = "././images/sun1.avif"
+}
